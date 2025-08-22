fa56b6a4bcae4b23592fd97bcae2293b79e23728
Lena <mlenabelise@gmail.com>
2025-08-22 15:04:24 +0200

    done with the contact page can always add some details tho

--- src/Controller/visitors/ContactPageController.php
+++ src/Controller/visitors/ContactPageController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller\visitors;
 
 use DateTime;
@@ -9,19 +10,39 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 
 class ContactPageController extends AbstractController
 {
-    #[Route(name:'Contact', path:'/contact')]
+    #[Route(name: 'Contact', path: '/contact')]
     public function contactDisplay(
         Request $request,
-        EntityManagerInterface $em
-        ):Response
-    {
+        EntityManagerInterface $em,
+        MailerInterface $mailer
+    ): Response {
         $contactMessage = new ContactMessage();
         $form = $this->createForm(ContactMessageForm::class, $contactMessage);
         $form->handleRequest($request);
-        if($form->isSubmitted()&&$form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // $data = $form->getData();
+
+            $email = (new Email())
+                ->from("no-reply@easypeasyfrance.fr")
+                ->replyTo($contactMessage->getEmail())
+                ->to('mlenabelise@gmail.com')
+                ->subject('New Contact Form Message')
+                ->text(
+                    "Name: {$contactMessage->getName()}\n" .
+                    "Email: {$contactMessage->getEmail()}\n" .
+                    "Subject: {$contactMessage->getSubject()}\n".
+                    "Message: {$contactMessage->getMessage()}"
+                );
+            $mailer->send($email);
+
+
             $em->persist($contactMessage);
             $contactMessage->setCreatedAt(new DateTime());
             $em->flush();
@@ -29,9 +50,8 @@ class ContactPageController extends AbstractController
             return $this->redirectToRoute('Contact');
         }
 
-        return $this-> render('marketingPages/contact.html.twig',[
-            "form"=>$form->createView()
+        return $this->render('marketingPages/contact.html.twig', [
+            "form" => $form->createView()
         ]);
-
     }
 }
