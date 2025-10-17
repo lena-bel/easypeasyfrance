7f1fb308f59768e781d1ed936a66c3f7b3f03599
Lena <mlenabelise@gmail.com>
2025-10-17 14:20:50 +0200

    user dashboard view

---
+++ src/Controller/UserDasboardController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserDasboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'user_dashboard')]
+    #[IsGranted('ROLE_USER')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        // basically double checking in case the is granted doesn't work
+        if (!$user) {
+            return $this->redirectToRoute('app_login'); 
+        }
+        // dd($user);
+
+        return $this->render('user-dashboard.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
