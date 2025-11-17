1c5eff7c5d039385561b75d96a3975417b3d4ad8
Lena <mlenabelise@gmail.com>
2025-11-17 11:07:02 +0100

    errors pages

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,25 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    protected function initializeContainer()
+    {
+        parent::initializeContainer();
+
+        // If debug mode is ON, Symfony uses the "blue screen"
+        // We disable it so custom error pages show even in dev.
+        if ($this->debug) {
+            restore_error_handler();
+            restore_exception_handler();
+
+            // Register the normal production error handler
+            ErrorHandler::register(null, false);
+        }
+    }
 }
